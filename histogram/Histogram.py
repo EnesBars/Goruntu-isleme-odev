7915f2e0c5fd91234f60cd0f933c07d8c167c855
Enes Barsbay <72464278+EnesBars@users.noreply.github.com>
2022-11-02 12:50:01 +0300

    Update Histogram.py

--- histogram/Histogram.py
+++ histogram/Histogram.py
@@ -2,20 +2,26 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-# resim okuma , sınır hesapla ve resimi goster
-resim = cv2.imread("candy.jpg")
-s = resim.shape
-cv2.imshow('resim', resim)
-#resimi gri yap ve göster
-resim_gray = cv2.cvtColor(resim, cv2.COLOR_BGR2GRAY)
-cv2.imshow('resim gri', resim_gray)
+
+# resim img at
+# shape ile sınırları s değişkenine at
+#resmi göster
+img = cv2.imread("candy.jpg")
+s = img.shape
+cv2.imshow('resim normal', img)
+
+#BGR den GRAY çevir renkleri
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gri hali', img_gray)
+
 #histogram hesapla
 H = np.zeros(shape=(256,1))
+#matris s değerinde
 for i in range(s[0]):
     for j in range(s[1]):
-        k = resim_gray[i,j]
+        k = img_gray[i,j]
         H[k,0] = H[k,0]+1
-#print(H)
+
 #histogram yazdir
 plt.plot(H)
 plt.show()
