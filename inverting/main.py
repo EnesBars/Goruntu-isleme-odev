d92fcd9af891abd170a2bebfcfff23077b221c50
Enes Barsbay <72464278+EnesBars@users.noreply.github.com>
2022-11-02 13:03:42 +0300

    Update main.py

--- inverting/main.py
+++ inverting/main.py
@@ -1,18 +1,24 @@
-#Enes Barsbay 02205076051
+# Enes Barsbay 02205076051
 import cv2
 import numpy as np
-#orjinal resimi al ve göster
-resim2 = cv2.imread('lena.jpg')
-cv2.imshow('resim', resim2)
-#işlenicek resim 
-resim = cv2.imread('lena.jpg',0)
-cv2.imshow('resim-0', resim)
-#resmi inverting et 
-[h, w] = resim.shape
+
+# orijinal resimi göstermek için olan kısım
+img2 = cv2.imread('lena.jpg')
+cv2.imshow('orijinal', img2)
+
+# işlenicek resim
+img = cv2.imread('lena.jpg', 0)
+cv2.imshow('resim-0', img)
+
+# shape ile resim boyutunda matris oluştur
+# np.zeros ile 0 ile dolu dizi dönder
+# dizide gezerek inverting işlemini gerçekleştir
+[h, w] = img.shape
 resim2 = np.zeros([h, w], dtype=np.uint8)
 for i in range(h):
     for j in range(w):
-        resim2[i, j] = 255 - resim[i, j]
-#işlenen resmi göster
-cv2.imshow("Ters resim", resim2)
+        img2[i, j] = 255 - img[i, j]
+
+# işlenen resmi göster
+cv2.imshow("Ters resim", img2)
 cv2.waitKey()
